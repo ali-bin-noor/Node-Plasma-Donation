844a8a87c8d0f6078d2e97c8b7cd933d5b587f57
Maheboob Ali Kankurti <mahebok1@amdocs.com>
2021-05-18 01:36:38 +0530

    user static data added

---
+++ src/staticData/user.js
@@ -0,0 +1,28 @@
+var user = 
+[
+    {
+    password: "123",
+    name: "Maheboob Kankuti",
+    email: "maheboobkankurti@gmail.com",
+    Mob: 8484079010,
+    age: 24,
+    weight: 55,
+        bloodGroup: "O",
+        address:{
+            country: "India",
+            area: "Paccha Peth",	
+            city: "Solapur",	
+            state: "Maharashtra",	
+            pincode: 413005,
+        },
+        gender: "male",
+        child: "No",
+        reportsDates:{
+            first: "02/03/21",
+            second: "06/03/21"
+        }
+    }
+
+]
+
+module.exports = user
