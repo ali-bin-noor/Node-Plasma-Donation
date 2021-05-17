af2d16a9f24e7360969983613fcf6c1eadb7198b
Maheboob Ali Kankurti <mahebok1@amdocs.com>
2021-05-18 01:24:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+
+const PORT=4000
+
+
+
+app.get('/', (req, res) => 
+{
+    res.send("hello world")
+}) 
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is up on port ${PORT}`)    
+})
